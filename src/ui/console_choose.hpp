97065ceab5589ca0f32d55bc5f764a6e8e9f6813
unknown <galera951@gmail.com>
2024-12-10 11:01:24 +0300

    Update ui return view

--- src/ui/console_choose.hpp
+++ src/ui/console_choose.hpp
@@ -6,32 +6,35 @@
 
 #include <xtd/console.h>
 
+using xtd::console;
+using xtd::console_key;
+
 class ConsoleChoose {
 public:
 	static void show(std::vector<std::pair<std::string, std::function<void()>>> handlers) {
 		std::vector<std::string> items;
-		std::transform(handlers.cbegin(), handlers.cend(), std::back_inserter(items), [](const std::pair<std::string, std::function<void()>>& p) {
-			return p.first;
-		});
+		for (const auto& handler : handlers)
+			items.push_back(handler.first);
+		items.push_back("Return");
 		
 		size_t curIdx = 0;
 		updateScreen(items, curIdx);
 		while (true) {
-			if (xtd::console::key_available()) {
-				switch (xtd::console::read_key().key()) {
-				case xtd::console_key::q:
-					return;
-				case xtd::console_key::up_arrow:
+			if (console::key_available()) {
+				switch (console::read_key().key()) {
+				case console_key::up_arrow:
 					(curIdx > 0) ? (--curIdx) : 0;
 					break;
-				case xtd::console_key::down_arrow:
+				case console_key::down_arrow:
 					(curIdx < items.size() - 1) ? (++curIdx) : 0;
 					break;
-				case xtd::console_key::enter:
-					xtd::console::clear();
+				case console_key::enter:
+					if (curIdx == handlers.size())
+						return;
+					console::clear();
 					handlers.at(curIdx).second();
-					xtd::console::write_line("Press any key to return.");
-					xtd::console::read_key();
+					console::write_line("Press any key to return.");
+					console::read_key();
 					break;
 				default:
 					break;
@@ -43,15 +46,16 @@ public:
 
 private:
 	static void updateScreen(std::vector<std::string> items, const size_t idx) {
-		xtd::console::clear();
+		console::clear();
 		for (size_t i = 0; i < items.size(); ++i) {
 			auto item = items.at(i);
+			if (i == items.size() - 1)
+				console::write_line();
 			if (i == idx) {
-				xtd::console::write_line("-> {}", item);
+				console::write_line("-> {}", item);
 				continue;
 			}
-			xtd::console::write_line("   {}", item);
+			console::write_line("   {}", item);
 		}
-		xtd::console::write_line("\nPress 'q' to exit.");
 	}
 };
